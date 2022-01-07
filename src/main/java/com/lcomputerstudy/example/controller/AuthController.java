f0b69b45e7a4280a5ee48ba16517bc736989ed8b
cheoneunjeong <92387264+cheoneunjeong@users.noreply.github.com>
2022-01-07 19:52:16 +0900

    add controller

--- src/main/java/com/lcomputerstudy/example/controller/AuthController.java
+++ src/main/java/com/lcomputerstudy/example/controller/AuthController.java
@@ -1,10 +1,20 @@
 package com.lcomputerstudy.example.controller;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -14,6 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lcomputerstudy.example.config.JwtUtils;
+import com.lcomputerstudy.example.domain.User;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -35,7 +46,52 @@ public class AuthController {
 	UserService userService;
 	
 	@PostMapping("/signin")
-	public ResponseEntity<?> authenticateUser(@Validated @RequestBody)
+	public ResponseEntity<?> authenticateUser(@Validated @RequestBody LoginRequest loginRequest) {
+		
+		logger.info("test"+ loginRequest);
+		
+		Authentication authentication = authenticationManager.authenticate(
+				new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+		
+		SecurityContextHolder.getContext().setAuthentication(authentication);
+		String jwt = jwtUtils.generateJwtToken(authentication);
+		
+		User user = (User) authentication.getPrincipal();
+		logger.info("dddd"+ authentication.getPrincipal());
+		List<String> roles= user.getAuthorities().stream()
+				.map(item -> item.getAuthority())
+				.collect(Collectors.toList());
+		
+		return ResponseEntity.ok(new JwtResponse(jwt,
+												user.getUsername(),
+												user.getName(),
+												roles));
+	}
+	
+	@PostMapping("/signup")
+	public ResponseEntity<?> signupUser(@Validated @RequestBody JoinRequest joinRequest) {
+		
+		String encodedPassword = new BCryptPasswordEncoder().encode(joinRequest.getPassword());
+		
+		User user = new User();
+		
+		user.setUsername(joinRequest.getUsername());
+		user.setName(joinRequest.getName);
+		user.setPassword(encodedPassword);
+		user.setPhone(joinRequest.getPhone());
+		user.setAccountNonExpired(true);
+		user.setEnabled(true);
+		user.setAccountNonLocked(true);
+		user.setCredentialsNonExpired(true);
+		user.setAuthorities(AuthorityUtils.createAuthorityList("ROLE_USER"));
+		
+		userService.createUser(user);
+		
+		userService.createAuthority(user);
+		
+		return new ResponseEntity<>("success", HttpStatus.OK);
+		
+	}
 	
 
 }
