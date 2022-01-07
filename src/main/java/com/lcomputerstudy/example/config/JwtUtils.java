ca1c514289f2c5fbdd3d447d8885396fc9df2cce
cheoneunjeong <92387264+cheoneunjeong@users.noreply.github.com>
2022-01-07 21:24:16 +0900

    add jwtUtiles

--- src/main/java/com/lcomputerstudy/example/config/JwtUtils.java
+++ src/main/java/com/lcomputerstudy/example/config/JwtUtils.java
@@ -1,9 +1,20 @@
 package com.lcomputerstudy.example.config;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import com.lcomputerstudy.example.domain.User;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
 import lombok.RequiredArgsConstructor;
 
 @Component
@@ -18,5 +29,33 @@ public class JwtUtils {
 	
 	public String generateJwtToken(Authentication authentication) {
 		
+		User user = (User) authentication.getPrincipal();
+		
+		return Jwts.builder()
+				.setSubject((user.getUsername()))
+				.setIssuedAt(new Date())
+				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+				.signWith(SignatureAlgorithm.HS512, jwtSecret)
+				.compact();
+
+	}
+	
+	public boolean validateJwtToken(String authToken) {
+		try {
+			Jwts.parser().setSigningKey(jwtSecret).parseClaimsJwt(authToken);
+			return true;
+		} catch (SignatureException e) {
+			logger.error("Invalid JWT signature: {}", e.getMessage());
+		} catch (MalformedJwtException e) {
+			logger.error("Invalid JWT token: {}", e.getMessage());
+		} catch (ExpiredJwtException e) {
+			logger.error("JWT token is expired: {}", e.getMessage());
+		} catch (UnsupportedJwtException e) {
+			logger.error("JWT token is unsupported: {}", e.getMessage());
+		} catch (IllegalArgumentException e) {
+			logger.error("JWT claims string is empty: {}", e.getMessage());
+		}
+		
+		return false;
 	}
 }
